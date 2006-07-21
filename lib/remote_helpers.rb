eedc148c5aedcb5ac800eadc3be2b68629a50740
Andrew Kaspick <andrew@redlinesoftware.com>
2006-07-21 23:59:57 +0000

    added rdoc

--- lib/remote_helpers.rb
+++ lib/remote_helpers.rb
@@ -1,5 +1,21 @@
-# put the defaults in a seperate namespace
-# shorter and more related path name for overiding the options
+# All remote actions are automatically enabled to show a gui indicator during a remote call with this plugin.
+#
+# == Note
+#
+# If you have installed another plugin that overrides rails methods such as <tt>remote_function</tt>,
+# <tt>form_remote_tag</tt>, or <tt>submit_to_remote</tt> then this plugin may not work as intended. So if
+# you find that some methods aren't working as expected then this could be a possible problem.  This also
+# holds for the other plugins overriding the same methods.
+
+# Default values that can be overridden in <tt>environment.rb</tt>
+#
+# * <tt>default_image</tt> - The default image indicator
+# * <tt>default_id</tt> - The default css id given to the indicator image
+#
+# == Examples
+#
+# * <tt>RemoteIndicator.default_image = 'spinner.gif'</tt>
+# * <tt>RemoteIndicator.default_id = 'spinner'</tt>
 class RemoteIndicator
   @@default_image = 'indicator.gif'
   cattr_accessor :default_image
@@ -10,8 +26,23 @@ end
 
 module ActionView::Helpers::PrototypeHelper
 
-  def indicator(options = {:id => RemoteIndicator.default_id})
-    options.reverse_merge! :hide => true
+  # Creates an indicator image.  The options supplied are the same used with +image_tag+
+  #
+  # Current and additional options are:
+  # * <tt>:id</tt> - The css id of the indicator image.  Defaults to <tt>RemoteIndicator.default_id</tt>
+  # * <tt>:hide</tt> - Hide the image by default.  Defaults to true.
+  #
+  # === Examples
+  # 
+  # Using a custom indicator id
+  #   <%= indicator :id => 'spinner' %>
+  #
+  # Using many indicators on the same page 
+  #   <% collection.each do |id| %>
+  #     <%= link_to_remote :url => {:action => 'dosomething'}, :indicator => "link#{id}" %> <%= indicator :id => "link#{id}" %>
+  #   <% end %>
+  def indicator(options = {})
+    options.reverse_merge!(:id => RemoteIndicator.default_id, :hide => true)
 
     options[:style] = [options[:style], 'display:none'].compact.join(';') if options.delete(:hide)
 
@@ -20,8 +51,24 @@ module ActionView::Helpers::PrototypeHelper
 
   alias :remote_function_old :remote_function
 
-  # Set the :indicator option to an id of an element to show and hide during a remote call.
-  # Set :indicator to false if no indicator is to be used (default is RemoteIndicator::DEFAULT_INDICATOR_ID).
+  # Adds the functionality of using a remote indicator (an image) during the execution
+  # of all remote functions.
+  #
+  # Additional options:
+  # * <tt>:indicator</tt> - The css id of an element to show and hide during a remote call.
+  # Defaults to <tt>RemoteIndicator.default_id</tt>.
+  # Set :indicator to false if no indicator is to be used.
+  #
+  # === Examples
+  #
+  # * To use the default values - <tt><%= remote_function :url => {:action => 'dosomething'} %> <%= indicator %></tt>
+  # * To disable the gui indicator - <tt><%= remote_function :url => {:action => 'dosomething'}, :indicator => false %></tt>
+  # * To use a custom :id - <tt><%= remote_function :url => {:action => 'dosomething'}, :indicator => 'custom' %> <%= indicator :id => 'custom' %></tt>
+  #
+  # ==== IMPORTANT GOTCHA
+  # Don't forget to use <tt><%= indicator %></tt> on the page with remote calls or the javascript will fail
+  # and your action won't complete (In development mode you will be shown an alert if the indicator has not been defined).
+  # This doesn't apply if you set the indicator to false... <tt>:indicator => false</tt>.
   def remote_function(options)
     options.reverse_merge! :indicator => RemoteIndicator.default_id
 
@@ -40,9 +87,22 @@ module ActionView::Helpers::PrototypeHelper
 
   alias :form_remote_tag_old :form_remote_tag
   
-  # The option :disable_form disables a form (specified using the :submit option)
-  # during a remote call if set to true (default).
-  # Set :disable_form to false to keep the form enabled during a remote form submission.
+  # Additional functionality added to disable the form by default during a remote call.
+  # This is useful to prevent a user from submitting a form twice while a remote call is in progress
+  # since the submit button will be disabled and therefore not clickable.
+  #
+  # Additional options:
+  # * <tt>:disable_form</tt> - Specifies if the form will disable or not during the remote call.
+  #   Defaults to true.
+  # Set :disable_form to false to keep the form enabled during a remote function call.
+  #
+  # === Examples
+  #
+  # Automatically disables a form (no additional options)
+  #   <%= form_remote_tag(:url => {:action => 'dosomething'}) %>
+  #
+  # Prevents the form from being disabled
+  #   <%= form_remote_tag(:url => {:action => 'dosomething'}, :disable_form => false) %>
   def form_remote_tag(options = {})
     options.reverse_merge! :disable_form => true
 
@@ -56,8 +116,23 @@ module ActionView::Helpers::PrototypeHelper
   alias :submit_to_remote_old :submit_to_remote
 
   # The option :disable_form disables a form (specified using the :submit option)
-  # during a remote call if set to true (default).
-  # Set :disable_form to false to keep the form enabled during a remote form submission.
+  
+  # Additional functionality added to disable a form (specified using the :submit option) by default during a remote call.
+  # This is useful to prevent a user from submitting a form twice while a remote call is in progress
+  # since the submit button will be disabled and therefore not clickable.
+  #
+  # Additional options:
+  # * <tt>:disable_form</tt> - Specifies if a form will disable or not during the remote call.
+  #   Defaults to true.
+  # Set :disable_form to false to keep the form enabled during a remote function call.
+  #
+  # === Example
+  #
+  # Automatically disables a form (no additional options)
+  #   <%= submit_to_remote('name', 'Submit', :url => {:action => 'dosomething'}) %>
+  #
+  # Prevents a form from being disabled
+  #   <%= submit_to_remote('name', 'Submit', :submit => 'myform', :url => {:action => 'dosomething'}, :disable_form => false) %>
   def submit_to_remote(name, value, options = {})
     options.reverse_merge! :disable_form => true
 
